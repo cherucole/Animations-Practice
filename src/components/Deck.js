dc3d5c4ffa391189f749fce88d076b500d928129
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-06-15 14:04:16 +0300

    added pan responder to root component and moving it

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 const Deck = props => {
+  const position = new Animated.ValueXY();
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gesture) => {
+      position.setValue({ x: gesture.dx, y: gesture.dy });
+    },
+    onPanResponderRelease: () => {},
+  });
+
   const renderCards = () => {
     return props.data.map(item => {
       return props.renderCard(item);
     });
   };
-  return <View>{renderCards()}</View>;
+  return (
+    <Animated.View style={position.getLayout()} {...panResponder.panHandlers}>
+      {renderCards()}
+    </Animated.View>
+  );
 };
 
 export default Deck;
