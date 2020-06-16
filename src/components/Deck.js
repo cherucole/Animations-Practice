59a28b14f8aeaefd00e3259d54c1a94bb04a016f
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-06-16 16:23:00 +0300

    Added rotate to card using interpolation

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -11,13 +11,24 @@ const Deck = props => {
     onPanResponderRelease: () => {},
   });
 
+  const getCardStyle = () => {
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ['-120deg', '0deg', '120deg'],
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }], //similar to rotate: rotate but now using ES6
+    };
+  };
+
   const renderCards = () => {
     return props.data.map((item, index) => {
       if (index === 0) {
         return (
           <Animated.View
             key={item.id}
-            style={position.getLayout()}
+            style={getCardStyle()}
             {...panResponder.panHandlers}>
             {props.renderCard(item)}
           </Animated.View>
